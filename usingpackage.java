d49b6254a23d8149599943d6191de8f62dbb17c4
Omkar <omkar13021@example.com>
2022-08-05 10:19:06 +0530

    use package

---
+++ usingpackage.java
@@ -0,0 +1,10 @@
+import statment.*;
+public class usingpackage {
+    public static void main(String[] args) {
+        System.out.println("i am using ths package");
+        conditional1 g = new conditional1();
+        switch1 s = new switch1();
+        System.out.println(s);
+        System.out.println(g);
+    }
+}
