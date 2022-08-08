64d16e33c96dc01acdad4593e74e8b7674bdd089
Omkar <omkar13021@example.com>
2022-08-08 15:25:31 +0530

    time api

---
+++ Advance java/timeapi.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class timeapi {
+    public static void main(String[] args) {
+        LocalTime lt = LocalTime.now();
+        System.out.println(lt);
+    }
+}
